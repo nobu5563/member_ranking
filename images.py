f8d080b57a0396ecdbe3571cac3f32f6b6a5cc8e
nobu5563 <nobu.7231snsd@fuji.waseda.jp>
2020-12-23 06:56:33 +0900

    初回実行時は画像を保存するように修正

--- images.py
+++ images.py
@@ -1,3 +1,4 @@
+import os
 from PIL import Image
 import glob
 import matplotlib.pyplot as plt
@@ -12,22 +13,25 @@ def show_images(group_name):
     im_list = []
 
     for file_name in files:
-        print(file_name)
         img = Image.open(file_name)
         im_list.append(img)
-    concat_image(im_list, path)
+    concat_image(im_list, path, group_name)
 
 
 
-def concat_image(im_list, path):
+def concat_image(im_list, path, group_name):
     canvas = Image.new('RGB', (334*4, 500))
     size = 0
     for i in range(len(im_list)):
         canvas.paste(im_list[i], (size, 0))
         size += 334
 
-    canvas.show()
-    # if path is None:
-    #     canvas.save('image/{group_name}/all.png')
-    # 一括で表示したい
-    # 初回にその画像を保存して、次回からそれを読み込む
+    canvas.show() # 画像を並べて表示
+
+    # 初回はその画像を保存
+    if os.path.isfile(path) is False:
+        # save_path = f'image/{group_name}'
+        canvas.save(f'image/{group_name}/all.png')
+
+    # メンバーの名前も付けたい
+    # concat_imageを一般化
