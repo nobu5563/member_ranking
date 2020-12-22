742c5f3a1d9b8c83f7b403a555b81998914b6b75
nobu5563 <nobu.7231snsd@fuji.waseda.jp>
2020-12-23 06:44:15 +0900

    メンバーを並べて表示する関数を追加

--- images.py
+++ images.py
@@ -1,14 +1,33 @@
 from PIL import Image
+import glob
+import matplotlib.pyplot as plt
 
 
 
-def show_images(group_name, name_1, name_2):
+def show_images(group_name):
 
-    filename_1 = f"image/{group_name}/{name_1}.png"
-    filename_2 = f"image/{group_name}/{name_2}.png"
+    path = f'image/{group_name}/all.png'
+    files = glob.glob(f'image/{group_name}/*.png')
 
-    im1 = Image.open(filename_1)
-    im2 = Image.open(filename_2)
+    im_list = []
 
-    im1.show()
-    im2.show()
+    for file_name in files:
+        print(file_name)
+        img = Image.open(file_name)
+        im_list.append(img)
+    concat_image(im_list, path)
+
+
+
+def concat_image(im_list, path):
+    canvas = Image.new('RGB', (334*4, 500))
+    size = 0
+    for i in range(len(im_list)):
+        canvas.paste(im_list[i], (size, 0))
+        size += 334
+
+    canvas.show()
+    # if path is None:
+    #     canvas.save('image/{group_name}/all.png')
+    # 一括で表示したい
+    # 初回にその画像を保存して、次回からそれを読み込む
